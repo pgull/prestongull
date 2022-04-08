a1d3d6e41e558674bd22b75a82f6db0a468509eb
pgull <preston@latitude.io>
2022-04-08 11:30:09 -0600

    hero type

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,39 @@
+import Slide from "react-reveal/Slide";
+import Fade from "react-reveal/Fade";
+
 const Home = () => {
   return (
-    <div className="page-wrap">
-      <div className="c-grid d-col-6 t-col-3">
-        <h1 className="display t-span-3">01</h1>
-        <h1 className="t-span-1">Header size</h1>
-        <p className="d-span-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-          bibendum nisi sit amet felis fermentum congue. Aliquam ullamcorper
-          ante facilisis sem lacinia, id gravida nisl aliquet. Etiam et ex id
-          justo tristique auctor. Aenean fermentum placerat quam, eu porttitor
-          magna luctus non. Suspendisse et malesuada massa, a imperdiet turpis.
-          Morbi rhoncus condimentum metus, vel fringilla nibh lobortis a. Nunc
-          id pulvinar nibh.
+    <div className="page-wrap light">
+      <div className="c-grid d-col-6">
+        <br className="d-span-2" />
+        <div className="display d-span-6" style={{ textAlign: "right" }}>
+          <h1 className="display">
+          <Slide bottom cascade>
+            Pres&shy;ton
+          </Slide>
+          </h1>
+          <h1 className="display">
+          <Slide bottom cascade delay={300}>
+            —Gull
+          </Slide>
+          </h1>
+        </div>
+        <div className="pg-logo-container">
+        <Slide bottom delay={150}>
+          <img src="https://res.cloudinary.com/preston-gull/image/upload/v1649438065/pg-logo_vxd4om.svg" alt="" className="pg-logo"/>
+        </Slide>
+        </div>
+        <Fade bottom delay={300}>
+        <p
+          className="d-span-2"
+          data-scroll
+          data-scroll-speed="3"
+          data-scroll-position="top"
+        >
+          I'm a designer in many media. Currently, I'm doing digital product
+          design for Latitude while in Asheville, USA.
         </p>
+        </Fade>
       </div>
     </div>
   );
