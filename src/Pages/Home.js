883b0d0d1ee23c324390bfbf3afada5f09e0c91d
pgull <preston@latitude.io>
2022-04-08 10:50:40 -0600

    grids

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
     <div className="page-wrap">
-      <div className="c-grid d-col-6">
-        <h1 className="display">01</h1>
-        <h1>Header size</h1>
+      <div className="c-grid d-col-6 t-col-3">
+        <h1 className="display t-span-3">01</h1>
+        <h1 className="t-span-1">Header size</h1>
         <p className="d-span-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
           bibendum nisi sit amet felis fermentum congue. Aliquam ullamcorper
