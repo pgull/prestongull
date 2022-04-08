5f4a5226a9783fd34f1d48eaa8451863e82a5e1d
pgull <preston@latitude.io>
2022-04-08 08:21:41 -0600

    humble beginnings

---
+++ src/PortfolioEntries.js
@@ -0,0 +1,24 @@
+export const portfolioEntries = {
+  'Hourglass': {
+    route: 'hourglass',
+    fullName: 'Hour —glass',
+    backdropImage: 'https://res.cloudinary.com/preston-gull/image/upload/v1649427142/sample.jpg',
+    theme: '#7A7F85',
+    page: <><h1>Hi</h1></>,
+  },
+  'Adobe': {
+    route: 'adobe',
+    fullName: 'Adobe —Inc.',
+    backdropImage: 'https://res.cloudinary.com/preston-gull/image/upload/v1649427142/sample.jpg',
+    theme: '#2D0202',
+    page: <></>,
+  },
+  'Scandinook': {
+    route: 'scandinook',
+    fullName: 'Scandi —nook',
+    backdropImage: 'https://res.cloudinary.com/preston-gull/image/upload/v1649427142/sample.jpg',
+    theme: '#0B1215',
+    page: <></>,
+  },
+  
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Pages/Home';
+import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import {portfolioEntries} from './PortfolioEntries'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="*" element={<App />} />
+      {Object.keys(portfolioEntries).map((key) => {
+        return <Route path={portfolioEntries[key].route} element={portfolioEntries[key].page} />
+      })}
+    </Routes>
+  </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
